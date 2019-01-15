5df76f856f9f12e657ee3da291efdd5c8776d089
zhuchao <479346874@qq.com>
2019-01-15 12:00:07 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,6 @@ Route::any('/pay/{order_id}','Pay\PayController@pay')->middleware('check.login.t
 Route::any('/pay/list/{order_id}','Pay\PayController@pay2')->middleware('check.login.token');
 Route::any('/alipay',' Alipay\alipayController@alipay')->middleware('check.login.token');
 
-Route::any('/alipay/{order_id}','Pay\AlipayController@pay')->middleware('check.login.token');
+Route::any('/alipay1/{order_id}','Pay\AlipayController@pay')->middleware('check.login.token');
 Route::post('/alipay2/notify','Pay\AlipayController@aliNotify');        //支付宝支付 异步通知回调
 Route::get('/alipay2/return','Pay\AlipayController@aliReturn');        //支付宝支付 同步通知回调
