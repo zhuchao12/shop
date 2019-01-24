864a2367736e703ae114cee19d94ad809082cc0b
zhuchao <479346874@qq.com>
2019-01-24 16:34:21 +0800

    aqwe

--- resources/views/movie/movie.blade.php
+++ resources/views/movie/movie.blade.php
@@ -1,1 +1,16 @@
-1
+@extends('layout.main')
+
+@section('content')
+    <h1>Movie</h1>
+    @foreach($seat as $k=>$v)
+        @if($v==1)
+            <button class="btn-default btn-danger">座位</button>
+        @else
+            <button class="btn-default btn-info">座位</button>
+        @endif
+    @endforeach
+
+@endsection
+
+@section('footer')
+@endsection
