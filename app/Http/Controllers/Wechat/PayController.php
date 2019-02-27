f557485087f24e399a501278f5024d7a370d557d
zhuchao <479346874@qq.com>
2019-02-27 14:57:41 +0800

    asdasd

--- app/Http/Controllers/Wechat/PayController.php
+++ app/Http/Controllers/Wechat/PayController.php
@@ -15,7 +15,8 @@ class PayController extends Controller
     public function test($order_id)
     {
         $total_fee = 1;         //用户要支付的总金额
-        $order_id = OrderModel::generateOrderSN();
+        //$order_id = OrderModel::generateOrderSN();
+        $orderInfo= OrderModel::where(['order_id'=>$order_id])->first();
 
         $order_info = [
             'appid'         =>  env('WEIXIN_APPID_0'),      //微信支付绑定的服务号的APPID
@@ -23,7 +24,7 @@ class PayController extends Controller
             'nonce_str'     => str_random(16),             // 随机字符串
             'sign_type'     => 'MD5',
             'body'          => '测试订单-'.mt_rand(1111,9999) . str_random(6),
-            'out_trade_no'  => $order_id,                       //本地订单号
+            'out_trade_no'  => $orderInfo->order_sn,                       //本地订单号
             'total_fee'     => $total_fee,
             'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],     //客户端IP
             'notify_url'    => $this->weixin_notify_url,        //通知回调地址
